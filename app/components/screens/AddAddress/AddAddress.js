6879697d6b2c7c1db0143c05e3d38acc48803a55
digholesnehal <snehal.dighole@neosofttech.com>
2018-08-28 19:39:44 +0530

    Completed with UI of Add Address screen

--- app/components/screens/AddAddress/AddAddress.js
+++ app/components/screens/AddAddress/AddAddress.js
@@ -14,6 +14,20 @@ import * as Colors from '../../../utils/colors';
 
 
 export default class AddAddress extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            address: '',
+            landmark: '',
+            city: '',
+            state: '',
+            zip: '',
+            country: '',
+            loader: false,
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -23,7 +37,51 @@ export default class AddAddress extends Component {
                     isDrawer={false}
                     isSearch={true}
                     back={() => { this.props.navigation.goBack(null) }} />
-
+                <View style={styles.container1}>
+                    <View style={styles.addressHead}>
+                        <Text style={styles.Headings}>ADDRESS</Text>
+                        <TextInput
+                            onChangeText={(changedText) => { this.setState({ "address": changedText }) }}
+                            value={this.state.address} style={styles.AddressTextField} multiline={true}>
+                        </TextInput>
+                        <Text style={styles.Headings}>LANDMARK</Text>
+                        <TextInput
+                            onChangeText={(changedText) => { this.setState({ "landmark": changedText }) }}
+                            value={this.state.landmark} style={styles.TextField}>
+                        </TextInput>
+                    </View>
+                    <View style={styles.addressMid}>
+                        <View style={styles.hrPartition}>
+                            <Text style={styles.Headings}>CITY</Text>
+                            <TextInput
+                                onChangeText={(changedText) => { this.setState({ "city": changedText }) }}
+                                value={this.state.city} style={styles.TextField}>
+                            </TextInput>
+                            <Text style={styles.Headings}>ZIP CODE</Text>
+                            <TextInput
+                                onChangeText={(changedText) => { this.setState({ "zip": changedText }) }}
+                                value={this.state.zip} style={styles.TextField}>
+                            </TextInput>
+                        </View>
+                        <View style={styles.hrPartition}>
+                            <Text style={styles.Headings}>STATE</Text>
+                            <TextInput
+                                onChangeText={(changedText) => { this.setState({ "state": changedText }) }}
+                                value={this.state.state} style={styles.TextField}>
+                            </TextInput>
+                            <Text style={styles.Headings}>COUNTRY</Text>
+                            <TextInput
+                                onChangeText={(changedText) => { this.setState({ "country": changedText }) }}
+                                value={this.state.country} style={styles.TextField}>
+                            </TextInput>
+                        </View>
+                    </View>
+                    <View style={styles.button}>
+                        <TouchableOpacity style={styles.buttonStyle} onPress={() => this.props.navigation.navigate('AddressList')}>
+                            <Text style={styles.btnTxt}> SAVE ADDRESS </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         )
     }
--- app/components/screens/AddAddress/Styles.js
+++ app/components/screens/AddAddress/Styles.js
@@ -6,5 +6,61 @@ export default styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
     },
-
+    container1: {
+        flex: 1,
+        flexDirection: 'column',
+        padding: 10,
+        paddingTop: 0,
+    },
+    addressHead: {
+        paddingLeft: 6.5,
+        paddingRight: 6.5,
+    },
+    Headings: {
+        fontSize: 17,
+        fontWeight: '600',
+        color: Colors.blackPrimary,
+        paddingTop: 27,
+        paddingBottom: 17,
+    },
+    AddressTextField: {
+        backgroundColor: Colors.primary,
+        height: 100,
+        fontSize: 17,
+        padding: 10,
+    },
+    addressMid: {
+        flexDirection: 'row',
+        paddingBottom: 13,
+    },
+    TextField: {
+        backgroundColor: Colors.primary,
+        height: 35,
+        fontSize: 17,
+        padding: 10,
+    },
+    hrPartition: {
+        flex: 1,
+        flexDirection: 'column',
+        padding: 6.5,
+        paddingTop: 0,
+    },
+    buttonStyle: {
+        backgroundColor: Colors.redBtnBG,
+        height: Platform.OS === 'ios' ? 50 : 40,
+        borderRadius: 5,
+        color: Colors.redBtnTxt,
+        fontSize: Platform.OS === 'ios' ? 80 : 70,
+        fontWeight: 'bold',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    btnTxt: {
+        color: Colors.primary,
+        fontSize: Platform.OS === 'ios' ? 25 : 25,
+        fontWeight: '500',
+    },
+    button: {
+        padding: 6.5,
+    },
 })
